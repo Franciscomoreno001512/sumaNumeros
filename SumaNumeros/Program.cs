e46e31f6db75904865c43f64342d0eade50c0855
francisco javier <fco-javier-moreno@hotmail.com>
2020-11-04 11:22:36 -0600

    se versiona el codigo

---
+++ SumaNumeros/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1 + 2;
+        }
+    }
+}
